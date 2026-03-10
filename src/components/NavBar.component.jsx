e903eac6d2fd45cf70dbb16ef8224673ac3d44aa
Dimmis5 <zhangdimeo@gmail.com>
2026-03-10 21:56:46 +0100

    taille de l'image

--- src/components/NavBar.component.jsx
+++ src/components/NavBar.component.jsx
@@ -24,7 +24,7 @@ function Navbar() {
       <div className={navBarStyles.leftSection}>
         <div className={navBarStyles.logoContainerLeft}>
           <Link href={"/"}>
-            <Image src="/assets/INVEST.png" width={80} height={120} alt="logo" priority />
+            <Image src="/assets/INVEST.png" width={100} height={130} alt="logo" priority />
           </Link>
         </div>
 
